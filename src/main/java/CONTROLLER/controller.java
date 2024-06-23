2243d1517e57265ece296dc663338038c613b277
Pedro Henrique Milani Vagula <milanipedrovagula@gmail.com>
2024-06-23 14:59:09 -0300

    1

--- src/main/java/CONTROLLER/controller.java
+++ src/main/java/CONTROLLER/controller.java
@@ -1,13 +1,80 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package CONTROLLER;
+
+import MODELO.departamentos.departamento;
+import MODELO.funcionarios.docentes.docente;
+import MODELO.funcionarios.docentes.docenteEfetivo;
+import MODELO.funcionarios.tecnicos.tecnico;
+import MODELO.universidade.universidade;
+import MODELO.funcionarios.funcionarios;
 
 /**
  *
  * @author Pedro
  */
 public class controller {
+    private universidade Uni = new universidade();
     
+    public void addTecnico(String codigo, String nome, double salario, String nivel, tecnico.FuncaoTecnico funcao) {
+        Uni.adicionarTecnico(codigo, nome, salario, nivel, funcao);
+    }
+    
+    public void addDocEfet(String codigo, String nome, double salario, String nivel, docente.TitulacaoDocente titulacao, docenteEfetivo.AreaDocente area) {
+        Uni.adicionarDocenteEfetivo(codigo, nome, salario, nivel, titulacao, area);
+    }
+    
+    public void addDocSub(String codigo, String nome, double salario, String nivel, docente.TitulacaoDocente titulacao, int cargahoraria) {
+        Uni.adicionarDocenteSubstituto(codigo, nome, salario, nivel, titulacao, cargahoraria);
+    }
+    
+    public String getRelatorioFunc() {
+        return Uni.relatorioFuncionarios();
+    }
+
+    public String getResumoDepartamentos() {
+        return Uni.resumoDepartamentos();
+    }
+
+    public String getResumoDepartamentosGastoFaixa(double min, double max) {
+        return Uni.resumoDepartamentosGastoFaixa(min, max);
+    }
+
+    public String getFuncionariosSalarioFaixa(double min, double max) {
+        return Uni.funcionariosSalarioFaixa(min, max);
+    }
+
+    public String getExibirTodosFuncionarios() {
+        return Uni.exibirTodosFuncionarios();
+    }
+
+    public String getExibirTodosTecnicos() {
+        return Uni.exibirTodosTecnicos();
+    }
+
+    public String getExibirTodosDocentes() {
+        return Uni.exibirTodosDocentes();
+    }
+
+    public String getExibirTodosDocentesEfetivos() {
+        return Uni.exibirTodosDocentesEfetivos();
+    }
+
+    public String getExibirTodosDocentesSubstitutos() {
+        return Uni.exibirTodosDocentesSubstitutos();
+    }
+
+    public String getExibirInformacoesDepartamento(String codigoDepartamento) {
+        return Uni.exibirInformacoesDepartamento(codigoDepartamento);
+    }
+
+    public funcionarios buscarFuncionarioCodigo(String codigo) {
+        return Uni.buscarFuncionarioCodigo(codigo);
+    }
+
+    public funcionarios buscarFuncionarioNome(String nome) {
+        return Uni.buscarFuncionarioNome(nome);
+    }
+
+    public void addDepartamento(departamento depto) {
+        Uni.adicionarDepartamento(depto);
+    }
 }
--- src/main/java/MODELO/universidade/universidade.java
+++ src/main/java/MODELO/universidade/universidade.java
@@ -1,59 +1,186 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package MODELO.universidade;
 
 import DATABASE.database;
+import MODELO.departamentos.departamento;
 import MODELO.funcionarios.docentes.docente;
 import MODELO.funcionarios.docentes.docenteEfetivo;
 import MODELO.funcionarios.funcionarios;
 import MODELO.funcionarios.tecnicos.tecnico;
 import MODELO.funcionarios.docentes.docenteSubstituto;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Pedro
  */
 public class universidade {
     private database db = database.getInstance();
-    private String nome;
-    
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-    
-    
-    
+    private List<departamento> departamentos = new ArrayList<>();
     
     public void adicionarTecnico(String codigo, String nome, double salario, String nivel, tecnico.FuncaoTecnico funcao) {        
         tecnico f = new tecnico(codigo, nome, salario, nivel, funcao);
         db.addFunc(f);
     }
     
-     public void adicionarDocenteEfetivo(String codigo, String nome, double salario, String nivel, docente.TitulacaoDocente titulacao, docenteEfetivo.AreaDocente area) {        
+    public void adicionarDocenteEfetivo(String codigo, String nome, double salario, String nivel, docente.TitulacaoDocente titulacao, docenteEfetivo.AreaDocente area) {        
         docenteEfetivo f = new docenteEfetivo(codigo, nome, salario, nivel, titulacao, area);
         db.addFunc(f);
     }
      
-      public void adicionarDocenteSubstituto(String codigo, String nome, double salario, String nivel, docente.TitulacaoDocente titulacao, int cargahoraria) {        
+    public void adicionarDocenteSubstituto(String codigo, String nome, double salario, String nivel, docente.TitulacaoDocente titulacao, int cargahoraria) {        
         docenteSubstituto f = new docenteSubstituto(codigo, nome, salario, nivel, titulacao, cargahoraria);
         db.addFunc(f);
     }
       
-      
- // olhar lista de coisas p/ fazer
     public String relatorioFuncionarios() {
         StringBuilder relatorio = new StringBuilder();
         funcionarios funcionarios[] = db.getFuncionarios();
         int qntd = db.getQuantidadeFuncionarios();
         for(int i = 0; i < qntd; i++) {
-            relatorio.append(funcionarios[i].toString());
+            relatorio.append(funcionarios[i].toString()).append("\n");
         }
         return relatorio.toString();
     }
-    
+
+    public String resumoDepartamentos() {
+        StringBuilder resumo = new StringBuilder();
+        for (departamento depto : departamentos) {
+            int quantidadeFuncionarios = depto.getFuncionarios().length;
+            double gastoTotal = 0;
+            for (funcionarios func : depto.getFuncionarios()) {
+                if (func != null) {
+                    gastoTotal += func.calcularSalario();
+                }
+            }
+            resumo.append("Departamento: ").append(depto.getNome())
+                  .append(", Quantidade de Funcionários: ").append(quantidadeFuncionarios)
+                  .append(", Gasto Total: ").append(gastoTotal).append("\n");
+        }
+        return resumo.toString();
+    }
+
+    public String resumoDepartamentosGastoFaixa(double min, double max) {
+        StringBuilder resumo = new StringBuilder();
+        for (departamento depto : departamentos) {
+            double gastoTotal = 0;
+            for (funcionarios func : depto.getFuncionarios()) {
+                if (func != null) {
+                    gastoTotal += func.calcularSalario();
+                }
+            }
+            if (gastoTotal >= min && gastoTotal <= max) {
+                resumo.append("Departamento: ").append(depto.getNome())
+                      .append(", Gasto Total: ").append(gastoTotal).append("\n");
+            }
+        }
+        return resumo.toString();
+    }
+
+    public String funcionariosSalarioFaixa(double min, double max) {
+        StringBuilder relatorio = new StringBuilder();
+        funcionarios funcionarios[] = db.getFuncionarios();
+        int qntd = db.getQuantidadeFuncionarios();
+        for(int i = 0; i < qntd; i++) {
+            if (funcionarios[i].getSalario() >= min && funcionarios[i].getSalario() <= max) {
+                relatorio.append(funcionarios[i].toString()).append("\n");
+            }
+        }
+        return relatorio.toString();
+    }
+
+    public String exibirTodosFuncionarios() {
+        return relatorioFuncionarios();
+    }
+
+    public String exibirTodosTecnicos() {
+        StringBuilder relatorio = new StringBuilder();
+        funcionarios funcionarios[] = db.getFuncionarios();
+        int qntd = db.getQuantidadeFuncionarios();
+        for(int i = 0; i < qntd; i++) {
+            if (funcionarios[i] instanceof tecnico) {
+                relatorio.append(funcionarios[i].toString()).append("\n");
+            }
+        }
+        return relatorio.toString();
+    }
+
+    public String exibirTodosDocentes() {
+        StringBuilder relatorio = new StringBuilder();
+        funcionarios funcionarios[] = db.getFuncionarios();
+        int qntd = db.getQuantidadeFuncionarios();
+        for(int i = 0; i < qntd; i++) {
+            if (funcionarios[i] instanceof docente) {
+                relatorio.append(funcionarios[i].toString()).append("\n");
+            }
+        }
+        return relatorio.toString();
+    }
+
+    public String exibirTodosDocentesEfetivos() {
+        StringBuilder relatorio = new StringBuilder();
+        funcionarios funcionarios[] = db.getFuncionarios();
+        int qntd = db.getQuantidadeFuncionarios();
+        for(int i = 0; i < qntd; i++) {
+            if (funcionarios[i] instanceof docenteEfetivo) {
+                relatorio.append(funcionarios[i].toString()).append("\n");
+            }
+        }
+        return relatorio.toString();
+    }
+
+    public String exibirTodosDocentesSubstitutos() {
+        StringBuilder relatorio = new StringBuilder();
+        funcionarios funcionarios[] = db.getFuncionarios();
+        int qntd = db.getQuantidadeFuncionarios();
+        for(int i = 0; i < qntd; i++) {
+            if (funcionarios[i] instanceof docenteSubstituto) {
+                relatorio.append(funcionarios[i].toString()).append("\n");
+            }
+        }
+        return relatorio.toString();
+    }
+
+    public String exibirInformacoesDepartamento(String codigoDepartamento) {
+        for (departamento depto : departamentos) {
+            if (depto.getCodigo().equals(codigoDepartamento)) {
+                StringBuilder info = new StringBuilder();
+                info.append("Departamento: ").append(depto.getNome()).append("\n");
+                info.append("Funcionários:\n");
+                for (funcionarios func : depto.getFuncionarios()) {
+                    if (func != null) {
+                        info.append(func.toString()).append("\n");
+                    }
+                }
+                return info.toString();
+            }
+        }
+        return "Departamento não encontrado.";
+    }
+
+    public funcionarios buscarFuncionarioCodigo(String codigo) {
+        funcionarios funcionarios[] = db.getFuncionarios();
+        int qntd = db.getQuantidadeFuncionarios();
+        for(int i = 0; i < qntd; i++) {
+            if (funcionarios[i].getCodigo().equals(codigo)) {
+                return funcionarios[i];
+            }
+        }
+        return null;
+    }
+
+    public funcionarios buscarFuncionarioNome(String nome) {
+        funcionarios funcionarios[] = db.getFuncionarios();
+        int qntd = db.getQuantidadeFuncionarios();
+        for(int i = 0; i < qntd; i++) {
+            if (funcionarios[i].getNome().equals(nome)) {
+                return funcionarios[i];
+            }
+        }
+        return null;
+    }
+
+    public void adicionarDepartamento(departamento depto) {
+        departamentos.add(depto);
+    }
 }
